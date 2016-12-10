445481c12c4628f2e19f5ea0ac7e444d92780d41
Kees Kluskens <kees@webduck.nl>
2016-12-10 11:44:31 +0100

    refactor shit

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         bundle: './client/app.js',
     },
     output: {
+        filename: '[name].js',
         path: path.join(__dirname, 'dist'),
     },
     resolve: {
         modules: [path.join(__dirname, 'client'), 'node_modules'],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'client/index.html'),
+        }),
+    ],
     devServer: {
-        historyApiFallback: true,
         proxy: {
             '/ws': {
                 target: 'http://127.0.0.1:3000/',
